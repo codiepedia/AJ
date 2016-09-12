65ff510f7538e86752c7c5e178f1d933c82650c7
yafengwang <yw2582@columbia.edu>
2016-09-11 17:32:46 -0700

    QuickFind

---
+++ src/Week1/QuickFind.java
@@ -0,0 +1,51 @@
+package Week1;
+
+/**
+ * Created by yafengwang on 9/11/16.
+ *
+ * Eager approach to solve the network connectivity problem.
+ */
+public class QuickFind {
+
+    private int[] id;
+
+    public QuickFind(int N) {
+        /**
+         * Constructor for Union-Find data type, this initializes the id array which holds the connections
+         * @ input: N, number of total objects
+         */
+        id = new int[N];
+        for(int i=0 ; i < N ; i++) {
+            id[i] = i;
+        }
+    }
+
+    public void union(int p, int q) {
+        /**
+         * Merge component that contains p into component that contains q
+         */
+        for(int i=0 ; i < id.length ; i++) {
+            if(id[i] == p) {
+                id[i] = q;
+            }
+        }
+    }
+
+    public int find(int p) {
+        /**
+         * Find the identifier for the component which contains p
+         */
+        return id[p];
+    }
+
+    public boolean connected(int p, int q) {
+        /**
+         * Test if two objects are connected with each other
+         */
+        return id[p] == id[q];
+    }
+
+
+
+
+}
